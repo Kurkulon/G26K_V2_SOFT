145288f7ba35879adbfd7525fa7e8e84b87d9f1c
Fantex <fantex@inbox.ru>
2022-12-01 18:55:20 +0400

    +

--- G26K_2_V2/G26K2_V2_BOOT_LPC824/cpp/hardware.h
+++ G26K_2_V2/G26K2_V2_BOOT_LPC824/cpp/hardware.h
@@ -4,7 +4,7 @@
 #include "hw_conf.h"
 
 extern void InitHardware();
-extern void UpdateHardware();
-extern u16 GetCRC(const void *data, u32 len);
+//extern void UpdateHardware();
+//extern u16 GetCRC(const void *data, u32 len);
 
 #endif // HARDWARE_H__23_12_2013__11_37
